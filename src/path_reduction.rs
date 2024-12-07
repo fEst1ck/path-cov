d0207ddc5cab990fb4a1846e067561d7d402cd73
Zekun Wang <zekun.wang@uwaterloo.ca>
2024-12-07 08:13:54 -0500

    opt expect to unwrap

--- src/path_reduction.rs
+++ src/path_reduction.rs
@@ -1,19 +1,19 @@
-use std::{env, fmt::Debug, hash::Hash};
 use petgraph::algo::tarjan_scc;
 use rustc_hash::{FxHashMap, FxHashSet};
 use sha2::digest::block_buffer::Block;
+use std::{env, fmt::Debug, hash::Hash};
 
 use crate::{
     convert::GNFA,
     extern_cfg::{BlockID, FunID},
     intern_cfg::CFG,
-    re::{RegExp, ParseErr},
+    re::{ParseErr, RegExp},
 };
 
 const PATH_REDUCTION_DEBUG: &'static str = "PATH_REDUCTION_DEBUG";
 const PATH_REDUCTION_ON_ERROR: &'static str = "PATH_REDUCTION_ON_ERROR";
-const FULL_PATH : &'static str = "FULL_PATH";
-const EMPTY_PATH : &'static str = "EMPTY_PATH";
+const FULL_PATH: &'static str = "FULL_PATH";
+const EMPTY_PATH: &'static str = "EMPTY_PATH";
 
 pub struct PathReducer<BlockID, FunID> {
     res: FxHashMap<FunID, RegExp<BlockID, FunID>>,
@@ -25,7 +25,9 @@ pub struct PathReducer<BlockID, FunID> {
     k: usize,
 }
 
-impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash + Debug> PathReducer<BlockID, FunID> {
+impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash + Debug>
+    PathReducer<BlockID, FunID>
+{
     pub fn reduce(&self, mut path: &[BlockID], _cfg: FunID) -> Vec<BlockID> {
         if self.k == 42 {
             // println!("reducing path {:?}", path);
@@ -37,7 +39,10 @@ impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash 
         if path.is_empty() {
             return Vec::new();
         }
-        let cfg = self.firsts.get(&path[0]).expect(&format!("no fun starts with {:?}", path[0]));
+        let cfg = self
+            .firsts
+            .get(&path[0]).unwrap();
+            // .expect(&format!("no fun starts with {:?}", path[0]));
         // let re = self.res.get(&cfg).expect("invalid fun_id");
         let re = RegExp::Var(cfg.clone());
         let mut reduced_paths = Vec::new();
@@ -51,7 +56,7 @@ impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash 
                 }
                 Err(ParseErr::Abort(val)) => {
                     reduced_paths.append(&mut val.into_vec());
-                    return reduced_paths
+                    return reduced_paths;
                 }
                 Err(ParseErr::Invalid(s)) => {
                     if let Ok(on_error) = env::var(PATH_REDUCTION_ON_ERROR) {
@@ -76,7 +81,6 @@ impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash 
                         panic!("invalid path: {:?}, error: {}", unreduced, s);
                     }
                 }
-                
             }
         }
         reduced_paths
@@ -93,10 +97,17 @@ impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash 
     }
 
     fn get_last_blocks(&self, block: &BlockID) -> &FxHashSet<BlockID> {
-        self.lasts.get(block).expect(&format!("failed to get last blocks for block {:?}", block))
+        self.lasts
+            .get(block)
+            .expect(&format!("failed to get last blocks for block {:?}", block))
     }
 
-    fn simple_reduce_one_fun(&self, path: &mut &[BlockID], stack: &mut FxHashSet<BlockID>, skip: bool) -> Vec<BlockID> {
+    fn simple_reduce_one_fun(
+        &self,
+        path: &mut &[BlockID],
+        stack: &mut FxHashSet<BlockID>,
+        skip: bool,
+    ) -> Vec<BlockID> {
         // holds the reduced path of the current function call (including all sub-calls)
         let mut buffer = Vec::new();
         // maps a block to where it last appears in the buffer
@@ -107,7 +118,11 @@ impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash 
         } else {
             return buffer;
         };
-        let loop_heads = self.loop_heads.get(&first).expect(&format!("no loop heads for block {:?}", first));
+        let loop_heads = self
+            .loop_heads
+            .get(&first) // 1%
+            .unwrap();
+            // .expect(&format!("no loop heads for block {:?}", first));
         // read the first block
         *path = &path[1..];
         stack.insert(first.clone());
@@ -115,7 +130,7 @@ impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash 
             buffer.push(first.clone());
             // loop_stack.insert(first.clone(), 0);
         }
-        let lasts = self.get_last_blocks(&first);
+        let lasts = self.get_last_blocks(&first); //TODO: 6%
         // println!("first {:?} lasts {:?}", first, lasts);
         if lasts.contains(&first) {
             // the function contains only one block
@@ -128,16 +143,17 @@ impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash 
         loop {
             if let Some(block) = path.first().cloned() {
                 // block is the start of a new function
-                if self.firsts.contains_key(&block) {
+                if self.firsts.contains_key(&block) { // TODO: 5%
                     // the function is on stack
                     if skip || stack.contains(&block) {
                         self.simple_reduce_one_fun(path, stack, true);
                     } else {
                         // reduce the path of this function call
-                        buffer.append(&mut self.simple_reduce_one_fun(path, stack, skip));
+                        buffer.append(&mut self.simple_reduce_one_fun(path, stack, skip)); // TODO: 3%
                         // self.simple_reduce_one_fun(path, stack, skip, &mut buffer);
                     }
-                } else if lasts.contains(&block) { // we reach the end of the current function call
+                } else if lasts.contains(&block) { // TODO: 4%
+                    // we reach the end of the current function call
                     *path = &path[1..];
                     // stack.remove(&first);
                     if !skip {
@@ -148,21 +164,23 @@ impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash 
                         assert!(buffer.is_empty());
                         return buffer;
                     }
-                } else { // another block in the current function call
+                } else {
+                    // another block in the current function call
                     if skip {
                         *path = &path[1..];
                         continue;
                     }
                     if !loop_heads.contains(&block) {
                         *path = &path[1..];
-                        buffer.push(block.clone());
+                        buffer.push(block.clone()); // TODO: 2%
                         continue;
                     }
                     // appears in the buffer at `last_off`
-                    if let Some(pos) = buffer.iter().rev().position(|x| x == &block) {
-                        // block is not in the buffer
-                        buffer.truncate(pos);
-                    }
+                    // if let Some(pos) = buffer.iter().rev().position(|x| x == &block) {
+                    //     // block is not in the buffer
+                    //     buffer.truncate(pos);
+                    // }
+                    Self::foo(&mut buffer, block.clone());
                     // if let Some(&last_off) = loop_stack.get(&block) {
                     //     // remove the blocks starting from `last_off`
                     //     // println!("before drain {:?}", buffer);
@@ -181,6 +199,14 @@ impl<BlockID: Eq + Clone + Hash + Hash + Debug, FunID: Eq + Clone + Hash + Hash 
             }
         }
     }
+
+    // #[inline(never)]
+    fn foo(buffer: &mut Vec<BlockID>, block: BlockID) {
+        if let Some(pos) = buffer.iter().rev().position(|x| x == &block) {
+            // block is not in the buffer
+            buffer.truncate(pos);
+        }
+    }
 }
 
 impl PathReducer<BlockID, FunID> {
@@ -193,23 +219,25 @@ impl PathReducer<BlockID, FunID> {
             let graph = &cfg.graph;
             let components = tarjan_scc(graph);
             let mut heads = FxHashSet::default();
-            for nodes in components.iter(){
+            for nodes in components.iter() {
                 // println!("node {:?}", node.len());
                 if nodes.len() == 1 {
                     continue;
                 }
-                
+
                 for node in vec![nodes[0]] {
                     let weight = graph.node_weight(node).unwrap();
                     match weight {
-                        crate::convert::Node::Literal(block) => { heads.insert(block.clone()); },
+                        crate::convert::Node::Literal(block) => {
+                            heads.insert(block.clone());
+                        }
                         crate::convert::Node::Var(_) => (),
                         crate::convert::Node::Extern => (),
                     }
                 }
             }
             // if *fun_id == 346 {
-                // println!("heads {:?} out of {:?}", heads.len(), graph.node_count());
+            println!("heads {:?} out of {:?}", heads.len(), graph.node_count());
             // }
             loop_heads.insert(*fun_id, heads);
         }
@@ -221,7 +249,10 @@ impl PathReducer<BlockID, FunID> {
             let first = re.first();
             let old = firsts.insert(first, fun_id.clone());
             if let Some(old_fun_id) = old {
-                panic!("functions {} {} both start with block {}", old_fun_id, fun_id, first);
+                panic!(
+                    "functions {} {} both start with block {}",
+                    old_fun_id, fun_id, first
+                );
             }
             fun_id_to_firsts.insert(*fun_id, first);
 
@@ -231,10 +262,19 @@ impl PathReducer<BlockID, FunID> {
             // println!("computing loop heads for {:?}", fun_id);
             // loop_heads.insert(first, re.loop_heads());
         }
-        Self { res, firsts, lasts, k, loop_heads: loop_heads.into_iter().map(|(fun_id, heads)| {
-            let first = fun_id_to_firsts.get(&fun_id).unwrap();
-            (*first, heads)
-        }).collect() }
+        Self {
+            res,
+            firsts,
+            lasts,
+            k,
+            loop_heads: loop_heads
+                .into_iter()
+                .map(|(fun_id, heads)| {
+                    let first = fun_id_to_firsts.get(&fun_id).unwrap();
+                    (*first, heads)
+                })
+                .collect(),
+        }
     }
 }
 
@@ -261,9 +301,27 @@ fn last_map(
     cfgs.iter()
         // .par_bridge()
         .map(|(_fun_id, cfg)| {
-            let first = cfg.graph.node_weight(cfg.entry).unwrap().clone().to_block_id();
-            let exit_node_indices: Vec<_> = cfg.graph.node_indices().filter(|node_idx| cfg.graph.neighbors(*node_idx).count() == 0).collect();
-            let exit_nodes = exit_node_indices.iter().map(|node_idx| cfg.graph.node_weight(*node_idx).unwrap().clone().to_block_id()).collect();
+            let first = cfg
+                .graph
+                .node_weight(cfg.entry)
+                .unwrap()
+                .clone()
+                .to_block_id();
+            let exit_node_indices: Vec<_> = cfg
+                .graph
+                .node_indices()
+                .filter(|node_idx| cfg.graph.neighbors(*node_idx).count() == 0)
+                .collect();
+            let exit_nodes = exit_node_indices
+                .iter()
+                .map(|node_idx| {
+                    cfg.graph
+                        .node_weight(*node_idx)
+                        .unwrap()
+                        .clone()
+                        .to_block_id()
+                })
+                .collect();
             (first, exit_nodes)
         })
         .collect()
